54d48eda5064f302c73b815d89f776776627fae0
Mimi Zotti <mimilettd@gmail.com>
2021-12-01 12:08:10 -0800

    Day 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# advent_of_code
+# Advent of Code 2021
+https://adventofcode.com/2021
--- day_one/sonar_sweep.rb
+++ day_one/sonar_sweep.rb
@@ -3,14 +3,9 @@ file = File.open(File.join(File.dirname(__FILE__), 'input.txt'))
 sonar_data = file.readlines.map { |line| line.chomp.to_i }
 measurements = { increased: 0, decreased: 0 }
 
-sonar_data[0..-1].each_with_index do |element, i|
-  if i == 0
-    false
-  elsif sonar_data[i-1] < sonar_data[i]
-    measurements[:increased] += 1
-  elsif sonar_data[i-1] > sonar_data[i]
-    measurements[:decreased] += 1
-  end
+sonar_data[0..-1].each_index do |i|
+  next if i == 0
+  sonar_data[i-1] < sonar_data[i] ? measurements[:increased] += 1 : measurements[:decreased] += 1
 end
 
 puts measurements
