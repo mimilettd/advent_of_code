afbf6430232126ec357ac19641866aa1bc91af3e
Mimi Le <mimilettd@gmail.com>
2021-12-01 20:05:49 -0800

    Part 2

--- day_one/sonar_sweep.rb
+++ day_one/sonar_sweep.rb
@@ -1,11 +1,17 @@
 file = File.open(File.join(File.dirname(__FILE__), 'input.txt'))
 
 sonar_data = file.readlines.map { |line| line.chomp.to_i }
-measurements = { increased: 0, decreased: 0 }
+measurements = { increased: 0, decreased: 0, same: 0 }
 
 sonar_data[0..-1].each_index do |i|
   next if i == 0
-  sonar_data[i-1] < sonar_data[i] ? measurements[:increased] += 1 : measurements[:decreased] += 1
+  if sonar_data[i-1..i+1].sum < sonar_data[i..i+2].sum
+    measurements[:increased] += 1
+  elsif sonar_data[i-1..i+1].sum > sonar_data[i..i+2].sum
+    measurements[:decreased] += 1
+  else
+    measurements[:same] += 1
+  end
 end
 
 puts measurements
